84eabf71610769c8d8942a12e11fa4e624d7f1b7
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2025-03-08 13:45:43 -0600

    correcion de cosillas

--- app/src/main/java/com/pipeanayap/foodapp/models/Categoria.kt
+++ app/src/main/java/com/pipeanayap/foodapp/models/Categoria.kt
@@ -17,6 +17,14 @@ val categoryList = listOf(
     Categoria(
         nombre = "Italian",
         imagen = "https://cdn4.iconfinder.com/data/icons/food-and-drink-1-2/64/pizza-food-italian-meal-512.png"
+    ),
+    Categoria(
+        nombre = "Mexican",
+        imagen = "https://png.pngtree.com/png-clipart/20220615/original/pngtree-mexican-food-tacos-vector-image-png-image_8043031.png"
+    ),
+    Categoria(
+        nombre = "Argentinian",
+        imagen = "https://images.vexels.com/media/users/3/298553/isolated/preview/fcbdc0164d098cf503912f651f2830e6-icono-de-empanada-de-comida-argentina.png"
     )
 
 )
--- app/src/main/java/com/pipeanayap/foodapp/models/Restaurante.kt
+++ app/src/main/java/com/pipeanayap/foodapp/models/Restaurante.kt
@@ -17,5 +17,12 @@ val restaurantList = listOf(
     Restaurante(
         nombre = "KFC",
         imagen = "https://cdn.freelogovectors.net/wp-content/uploads/2023/01/kfclogo-freelogovectors.net_.png"
-    )
+    ),
+    Restaurante(
+        nombre = "MR Q",
+        imagen = "https://www.mrqchinese.com/wp-content/uploads/2022/05/cropped-MR-Q-favicon.png"
+    ),
+    Restaurante(
+        nombre = "Ciao Pizza",
+        imagen = "https://static.wixstatic.com/media/803a1c_1d3539f3ffcb447ea06151e5b5718231~mv2.png/v1/fill/w_560,h_560,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Logo_CiaoCiaolg.png"    )
 )
